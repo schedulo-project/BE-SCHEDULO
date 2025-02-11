1859d9452c0c5d0c1fe9977a3db47f2ceb738b66
s2vngwxx <ph1losophe@naver.com>
2025-02-12 02:49:59 +0900

    feat: add models User, StudyRoutine

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = models.CharField(null=True, max_length=10)
+    email = models.EmailField(null=True)
+    first_name, last_name = None, None
+
+class StudyRoutine(models.Model):
+    weeks_before_exam = models.IntegerField(null=True)
+    review_type = models.CharField(null=True, max_length=30)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="study_routine")

2dd5bee38d4d18dd9c3c6bf5d677b3c72c04170a
s2vngwxx <ph1losophe@naver.com>
2025-02-21 02:53:41 +0900

    feat: implement TagListCreateAPIView, TagRetrieveUpdateDestroyAPIView

--- schedules/views.py
+++ schedules/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
 
-# Create your views here.
+from schedules.models import Tag, Schedule, TimeTable
+from schedules.serializers import TagSerializer, ScheduleSerializer, TimeTableSerializer
+
+# Tag 조회, 생성
+class TagListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = TagSerializer
+
+    def get_queryset(self):
+        return Tag.objects.filter(user=self.request.user)
+        
+# Tag 수정, 삭제
+class TagRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TagSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'tag_id'
+
+    def get_queryset(self):
+        return Tag.objects.filter(user=self.request.user)
+
+# Schedule 조회(캘린더 고려), 생성
+class ScheduleListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = ScheduleSerializer
+
+    def get_queryset(self):
+        return Schedule.objects.filter(user=self.request.user)
+    
+# Schedule 수정, 삭제, 연기
+class ScheduleRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ScheduleSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'schedule_id'
+
+    def get_queryset(self):
+        return Schedule.objects.filter(user=self.request.user)
+
+# TimeTable 조회, 생성, 수정

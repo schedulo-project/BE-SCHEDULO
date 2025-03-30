41552f667409b05db51e5ea3b2dd7e1fb076fa58
s2vngwxx <ph1losophe@naver.com>
2025-03-30 15:12:34 +0900

    feat: Update chatbots views following changed model

--- chatbots/views.py
+++ chatbots/views.py
@@ -2,7 +2,7 @@ import json
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Chatting
-from .serializers import ChattingSerializer, AnswerSerializer
+from .serializers import ChattingSerializer
 from .services import get_chatbot_response
 
 
@@ -16,12 +16,9 @@ class ChatbotAPIView(APIView):
         query = request.data.get("query")
         answer_json = get_chatbot_response(query)[7:-4]
         answer_dict = json.loads(answer_json)
-        answer_serializer = AnswerSerializer(data=answer_dict)
-        answer_serializer.is_valid(raise_exception=True)
-        answer = answer_serializer.save()
 
         chatting = Chatting.objects.create(
-            query=query, answer=answer, user=request.user
+            query=query, answer=answer_dict, user=request.user
         )
 
         serializer = ChattingSerializer(chatting)

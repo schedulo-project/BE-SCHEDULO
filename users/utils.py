59bd22fc13f1e0b66a99902f6d97f0e392b6cfb3
주현지 <hyjoo0909@naver.com>
2025-04-03 00:29:15 +0900

    feat: Add login attempt function for ecampus

--- users/utils.py
+++ users/utils.py
@@ -11,3 +11,23 @@ import re
 
 from schedules.models import Schedule, Tag, TimeTable
 from schedules.serializers import ScheduleSerializer
+
+
+def login_attempt(driver, USER_ID, USER_PW):
+    """ecampus 로그인 실행"""
+    driver.get("https://ecampus.smu.ac.kr/login.php")  # 로그인 페이지 URL
+    # 아이디, 비밀번호 입력란 찾기
+    username_input = WebDriverWait(driver, 2).until(
+        EC.presence_of_element_located((By.NAME, "username"))
+    )
+    password_input = WebDriverWait(driver, 2).until(
+        EC.presence_of_element_located((By.NAME, "password"))
+    )
+    username_input.send_keys(USER_ID)
+    password_input.send_keys(USER_PW)
+
+    # 로그인 버튼 클릭
+    login_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.NAME, "loginbutton"))
+    )
+    login_button.click()

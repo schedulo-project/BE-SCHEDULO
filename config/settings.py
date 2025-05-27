8ef21fa07905fa46a034e97d6ae75a0b60b780eb
주현지 <hyjoo0909@naver.com>
2025-05-27 13:42:35 +0900

    fix: update database configuration to use MySQL

--- config/settings.py
+++ config/settings.py
@@ -134,14 +134,20 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+DB_PASSSWORD = os.getenv("DB_PASSSWORD")
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "schedulo",
+        "USER": "root",
+        "PASSWORD": DB_PASSSWORD,
+        "HOST": "13.124.140.60",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

f2250bb8d7b94a032ef90c2037e6914f758f6cb7
s2vngwxx <ph1losophe@naver.com>
2025-05-28 15:52:02 +0900

    feat: Remove argument "request"

--- users/tasks.py
+++ users/tasks.py
@@ -4,8 +4,24 @@ from .models import User, Score
 from schedules.models import Schedule
 
 
+def calculate_score_by_ratio(ratio, schedules_count):
+    score = 0
+    if ratio < 0.6:
+        score -= 5
+    elif ratio < 0.8:
+        score += 5
+    else:
+        score += 10
+
+    if schedules_count >= 5:
+        score += 3
+        if schedules_count >= 10:
+            score += 5
+    return score
+
+
 @shared_task
-def calculate_score(request):
+def calculate_score():
     users = User.objects.all()
     score = 0
     score_one_day_ago = 0
@@ -40,20 +56,19 @@ def calculate_score(request):
             return
 
         ratio = completed_schedules_count / schedules_count
-
-        if ratio < 0.6:
-            score -= 5
-        elif ratio < 0.8:
-            score += 5
-        else:
-            score += 10
-
-        if schedules_count >= 5:
-            score += 3
-            if schedules_count >= 10:
-                score += 5
-
+        score += calculate_score_by_ratio(ratio, schedules_count)
         user_score = Score.objects.create(user=user, score=score, date=date.today())
         user_score.save()
 
+        update_user_percentages()
         return
+
+
+def update_user_percentages():
+    scores = list(Score.objects.filter(date=date.today()).order_by("-score"))
+    total = len(scores)
+
+    for index, score in enumerate(scores):
+        percentage = round((index / total * 100), 2)
+        score.percentage = percentage
+        score.save()

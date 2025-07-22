06075e2533576bd000f8a24c2e0ee635292c56f8
s2vngwxx <ph1losophe@naver.com>
2025-07-22 23:19:28 +0900

    feat: Add tag color attribute to Schedule update

--- schedules/views.py
+++ schedules/views.py
@@ -165,6 +165,10 @@ class ScheduleRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView
                 tag_instance, created = Tag.objects.get_or_create(
                     name=tag_name, user=request.user
                 )
+                if created:
+                    order = Tag.objects.filter(user=self.request.user).count()
+                    tag_instance.color = tag_colors[order % len(tag_colors)]
+                    tag_instance.save()
                 tag_instances.append(tag_instance)
             schedule.tag.set(tag_instances)
 
